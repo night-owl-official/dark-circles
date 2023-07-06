a21bc8dcba9c49585bc808ea41056d86d7ea9456
Daniel Grieco <daniel_grieco@programmer.net>
2023-07-06 02:27:13 +0200

    Implement cursors animations in the CursorManager script

--- Assets/Scripts/UI/CursorManager.cs
+++ Assets/Scripts/UI/CursorManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class CursorManager : MonoBehaviour {
     public enum CursorType {
@@ -9,11 +10,51 @@ public class CursorManager : MonoBehaviour {
     // Start is called before the first frame update
     private void Start() {
         Cursor.visible = true;
+
+        // Default is follow cursor
+        SetCurrentCursor(cursors[0]);
+    }
+
+    private void SetCurrentCursor(CursorSO cursorSO) {
+        Assert.IsNotNull(cursorSO);
+        currentCursor = cursorSO;
     }
 
     // Update is called once per frame
     private void Update() {
-        
+        RenderMouseCursor();
+    }
+
+    private void RenderMouseCursor() {
+        SetFrameIndex();
+        SetMouseCursor();
+    }
+
+    /// <summary>
+    /// Must be called every frame.
+    /// </summary>
+    private void SetFrameIndex() {
+        Assert.IsNotNull(currentCursor);
+
+        timer += Time.deltaTime;
+        float frameDuration = 1f / currentCursor.frameRate;
+
+        if (timer >= frameDuration) {
+            currentFrameIndex = (currentFrameIndex + 1) % currentCursor.animationFrames.Length;
+            timer -= frameDuration;
+        }
+    }
+
+    private void SetMouseCursor() {
+        Assert.IsNotNull(currentCursor);
+
+        Texture2D currentFrame = currentCursor.animationFrames[currentFrameIndex];
+
+        Vector2 hotspot = currentCursor.isHotspotMiddle ?
+                          new Vector2(currentFrame.width, currentFrame.height) * 0.5f :
+                          currentCursor.hotspot;
+
+        Cursor.SetCursor(currentFrame, hotspot, CursorMode.Auto);
     }
     #endregion
 
@@ -23,5 +64,7 @@ public class CursorManager : MonoBehaviour {
 
     private int currentFrameIndex = 0;
     private float timer = 0f;
+
+    private CursorSO currentCursor = null;
     #endregion
 }

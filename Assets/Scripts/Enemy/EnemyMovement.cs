29405c695415a50573af0c7d572830874ce221d3
Daniel Grieco <daniel_grieco@programmer.net>
2023-08-10 02:45:08 +0200

    Add function to make the enemy face the target's direction

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,6 +5,7 @@ public class EnemyMovement : MonoBehaviour {
     #region Methods
     // Start is called before the first frame update
     private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         target = FindObjectOfType<PlayerMovement>().transform;
     }
 
@@ -14,7 +15,23 @@ public class EnemyMovement : MonoBehaviour {
     }
 
     private void ChaseTarget() {
-        Assert.IsNotNull(target);
+        FaceTarget();
+        UpdatePositionBasedOnTarget();
+    }
+
+    private void FaceTarget() {
+        Assert.IsNotNull(target, "No target found on EnemyMovement");
+        Assert.IsNotNull(spriteRenderer, "No sprite renderer found on EnemyMovement");
+
+        if (target.position.x > transform.position.x) {
+            spriteRenderer.flipX = false;
+        } else {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void UpdatePositionBasedOnTarget() {
+        Assert.IsNotNull(target, "No target found on EnemyMovement");
 
         Vector3 directionToTarget = (target.position - transform.position).normalized;
         transform.position += speed * Time.deltaTime * directionToTarget;
@@ -25,6 +42,7 @@ public class EnemyMovement : MonoBehaviour {
     [SerializeField]
     private float speed = 1.0f;
 
+    private SpriteRenderer spriteRenderer;
     private Transform target;
     #endregion
 }

2e8c75fa87161101b064591b1e7b90e151ad351c
Daniel Grieco <daniel_grieco@programmer.net>
2023-08-14 17:58:43 +0200

    Create RandomWalkDungeonGenerator script to handle generating the dungeon positions using random walk algorithm

---
+++ Assets/Scripts/Map/RandomWalkDungeonGenerator.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomWalkDungeonGenerator : MonoBehaviour {
+    #region Methods
+    public void RunProceduralGeneration() {
+        HashSet<Vector2Int> floorPositions = RunRandomWalk();
+        foreach (var floor in floorPositions) {
+            Debug.Log(floor);
+        }
+    }
+
+    private HashSet<Vector2Int> RunRandomWalk() {
+        HashSet<Vector2Int> floorPositions = new();
+        Vector2Int currentPosition = startPosition;
+
+        for (int i = 0; i < iterations; ++i) {
+            HashSet<Vector2Int> path =
+                ProceduralGenerator.RandomWalk(currentPosition, walkLength);
+
+            floorPositions.UnionWith(path);
+
+            if (startRandomlyEachIteration) {
+                currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
+            }
+        }
+
+        return floorPositions;
+    }
+    #endregion
+
+    #region Member variables
+    [SerializeField]
+    private Vector2Int startPosition = Vector2Int.zero;
+
+    [SerializeField]
+    private int iterations = 10;
+    [SerializeField]
+    private int walkLength = 10;
+    [SerializeField]
+    private bool startRandomlyEachIteration = true;
+    #endregion
+}
